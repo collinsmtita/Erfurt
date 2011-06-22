a0e6170d30a298c54b67ab4accdcf1d89f7ce351
Andreas Wolf <andreas.wolf@ikt-werk.de>
2011-06-22 10:34:25 +0200

    [TASK] Add rudimentary memory store
    Currently, it's only possible to add and simply retrieve statements;
    SPARQL capabilities still have to be added, as well as removal of
    statements (and some other features).

---
+++ Classes/Store/Adapter/Memory.php
@@ -0,0 +1,381 @@
+<?php
+declare(ENCODING = 'utf-8') ;
+namespace Erfurt\Store\Adapter;
+
+/*                                                                        *
+ * This script belongs to the Erfurt framework.                           *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License as published by the Free   *
+ * Software Foundation, either version 2 of the License, or (at your      *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
+ * General Public License for more details.                               *
+ *                                                                        *
+ * You should have received a copy of the GNU Lesser General Public       *
+ * License along with the script.                                         *
+ * If not, see http://www.gnu.org/copyleft/gpl.html.                      *
+ *                                                                        */
+
+
+/**
+ * @author Andreas Wolf <andreas.wolf@ikt-werk.de>
+ * @license http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
+ */
+class Memory extends AbstractAdapter {
+
+	/**
+	 * The graphs available in this store
+	 *
+	 * @var \Erfurt\Domain\Model\Rdf\Graph[]
+	 */
+	protected $graphs = array();
+
+	/**
+	 * All statements in this store. The statements are stored by their hash key, which is computed from graph, subject, predicate and object
+	 *
+	 * @var array
+	 * @see hashStatement()
+	 */
+	protected $statements = array();
+
+	/**
+	 * The statements in this store, ordered by graph and encodede with their hash key
+	 *
+	 * @var array
+	 */
+	protected $statementsByGraph = array();
+
+	/**
+	 * All subjects indexed by their subject
+	 *
+	 * @var array
+	 */
+	protected $statementsBySubject = array();
+
+	/**
+	 * All subjects indexed by their predicate
+	 *
+	 * @var array
+	 */
+	protected $statementsByPredicate = array();
+
+	/**
+	 * All subjects indexed by their object
+	 *
+	 * @var array
+	 */
+	protected $statementsByObject = array();
+
+	/**
+	 * This method allows the backend to (re)initialize itself, e.g. when an import was done.
+	 */
+	public function init() {
+		// TODO: Implement init() method.
+	}
+
+	/**
+	 * Returns the prefix used by the store to identify blank nodes.
+	 *
+	 * @return string
+	 */
+	public function getBlankNodePrefix() {
+		// TODO: Implement getBlankNodePrefix() method.
+	}
+
+	/**
+	 *
+	 * @param string $graphIri
+	 * @param string $locator Either a URL or a absolute file name.
+	 * @param string $type One of:
+	 *		- 'auto' => Tries to detect the type automatically in the following order:
+	 *		   1. Detect XML by XML-Header => rdf/xml
+	 *		   2. If this fails use the extension of the file
+	 *		   3. If this fails throw an exception
+	 *		- 'xml'
+	 *		- 'n3' or 'nt'
+	 * @param boolean $stream Denotes whether $data contains the actual data.
+	 *
+	 * @throws Erfurt_Exception
+	 *
+	 * @return boolean On success
+	 */
+	public function importRdf($graphIri, $data, $type, $locator) {
+		// TODO: Implement importRdf() method.
+	}
+
+	/**
+	 *
+	 * @param string $graphIri
+	 * @param string $serializationType One of:
+	 *		- 'xml'
+	 *		- 'n3' or 'nt'
+	 * @param mixed $filename Either a string containing a absolute filename or null. In case null is given,
+	 * this method returns a string containing the serialization.
+	 *
+	 * @return string/null
+	 */
+	public function exportRdf($graphIri, $serializationType = 'xml', $filename = null) {
+		// TODO: Implement exportRdf() method.
+	}
+
+	/**
+	 * Fetches information about available graphs. This method does nothing in this adapter, as it keeps all graphs
+	 * in memory.
+	 *
+	 * @return
+	 */
+	protected function fetchGraphInfos() {
+		return $this->graphInfoCache;
+	}
+
+	/**
+	 * @param string $graphIri The Iri, which identifies the graph to look for.
+	 * @param boolean $useAc Whether to use access control or not.
+	 *
+	 * @return boolean Returns true if graph exists and is available for the user ($useAc === true).
+	 */
+	public function isGraphAvailable($graphIri) {
+		return isset($this->graphs[$graphIri]) && !empty($this->graphs[$graphIri]);
+	}
+
+	/**
+	 * Returns a graph by its IRI. Unlike in other adapters, the graph object will not be created if it is not cached.
+	 *
+	 * @return \Erfurt\Domain\Model\Rdf\Graph
+	 */
+	public function getGraph($graphIri) {
+		if (!isset($this->graphs[$graphIri])) {
+			// TODO throw exception
+		}
+		return $this->graphs[$graphIri];
+	}
+
+	/**
+	 * Creates a new empty graph (named graph) with the URI specified.
+	 *
+	 * @param string $graphIri
+	 * @param int $type
+	 * @return boolean true on success, false otherwise
+	 */
+	public function createGraph($graphIri, $type = \Erfurt\Store\Store::GRAPH_TYPE_OWL) {
+		// TODO: Check if this implementation really makes sense; maybe it could also be (largely) moved to AbstractAdapter
+		$baseIri = $graphIri;
+		$graphObject = $this->createGraphObject($type, $graphIri, $baseIri);
+
+		$this->graphs[$graphIri] = $graphObject;
+		return TRUE;
+	}
+
+	/**
+	 * @param string $graphIri The Iri, which identifies the graph.
+	 *
+	 * @throws Erfurt\Exception Throws an exception if no permission, graph not existing or deletion fails.
+	 */
+	public function deleteGraph($graphIri) {
+		if (!isset($this->graphs[$graphIri])) {
+			// TODO: throw exception
+		}
+		unset($this->graphs[$graphIri]);
+		// TODO remove statements for graph
+	}
+
+	/**
+	 * Returns the formats this store can import.
+	 *
+	 * @return  array
+	 */
+	public function getSupportedImportFormats() {
+		// TODO: Implement getSupportedImportFormats() method.
+	}
+
+	/**
+	 * Returns the formats this store can export.
+	 *
+	 * @return  array
+	 */
+	public function getSupportedExportFormats() {
+		// TODO: Implement getSupportedExportFormats() method.
+	}
+
+	/**
+	 * Executes a SPARQL ASK query and returns a boolean result value.
+	 *
+	 * @param string $graphIri
+	 * @param string $askSparql
+	 * @param boolean $useAc Whether to check for access control.
+	 */
+	public function sparqlAsk($query) {
+		// TODO: Implement sparqlAsk() method.
+	}
+
+	/**
+	 * @param string $query A string containing a sparql query
+	 * @param array $graphIris An additional array of graphIris to query against. If a non empty array is given, the
+	 * values in this array will overwrite all FROM and FROM NAMED clauses in the query. If the array contains no
+	 * element, the FROM and FROM NAMED is evaluated. If non of them is present, all available graphs are queried.
+	 * @param array Option array to push down parameters to adapters
+	 * feel free to add anything you want. put the store name in front for special options, but use macros
+	 *	  'result_format' => ['plain' | 'xml']
+	 *	  'timeout' => 1000 (in msec)
+	 * I included some define macros at the top of Store.php
+	 *
+	 * deprecated: @param string $resultform Currently supported are: 'plain' and 'xml'
+	 * @param boolean $useAc Whether to check for access control or not.
+	 *
+	 * @throws Erfurt_Exception Throws an exception if query is no string.
+	 *
+	 * @return mixed Returns a result depending on the query, e.g. an array or a boolean value.
+	 */
+	public function sparqlQuery($query, $options = array()) {
+		// TODO: Implement sparqlQuery() method.
+	}
+
+	protected function hashStatement($graphIri, $subject, $predicate, $object) {
+		$hash = md5(
+			$graphIri
+			. '|' . $subject
+			. '|' . $predicate
+			. '|' . $object
+		);
+		return $hash;
+	}
+
+	/**
+	 * Adds statements in an array to the graph specified by $graphIri.
+	 *
+	 * @param string $graphIri
+	 * @param array  $statementsArray
+	 * @param array  $options ("escapeLiteral" => true/false) to disable automatical escaping characters
+	 */
+	public function addMultipleStatements($graphIri, array $statementsArray, array $options = array()) {
+		if (!$this->isGraphAvailable($graphIri)) {
+			throw new Exception("Graph $graphIri does not exist. Can't add statements to it.", 1308322086);
+		}
+
+		foreach ($statementsArray as $subject => $predicatesArray) {
+			foreach ($predicatesArray as $predicate => $objectsArray) {
+				foreach ($objectsArray as $object) {
+					$hash = $this->hashStatement($graphIri, $subject, $predicate, $object);
+
+					$this->statements[$hash] = array(
+						'g' => $graphIri,
+						's' => $subject,
+						'p' => $predicate,
+						'o' => $object
+					);
+
+					$this->addToIndexTable($this->statementsByGraph, $graphIri, $hash);
+					$this->addToIndexTable($this->statementsBySubject, $subject, $hash);
+					$this->addToIndexTable($this->statementsByPredicate, $predicate, $hash);
+					$this->addToIndexTable($this->statementsByObject, $object, $hash);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Adds a hash entry to a two-dimensional index table. This is used for storing the graph, subject, predicate and object
+	 * hashes.
+	 */
+	protected function addToIndexTable(&$indexTable, $key, $hash) {
+		if (!isset($indexTable[$key])) {
+			$indexTable[$key] = array();
+		}
+
+		$indexTable[$key][] = $hash;
+	}
+
+	protected function removeFromIndexTable(&$indexTable, $key, $hash) {
+		if (!in_array($hash, $indexTable[$key])) {
+			return;
+		}
+
+		$index = array_search($hash, $indexTable[$key]);
+		array_splice($indexTable, $index, 1, array());
+	}
+
+	protected function intersectWithoutNullValues() {
+		$intersectionArguments = array();
+		foreach (func_get_args() as $argument) {
+			if (is_array($argument)) {
+				$intersectionArguments[] = $argument;
+			}
+		}
+
+		if (count($intersectionArguments) == 0) {
+			return 0;
+		} elseif (count($intersectionArguments) == 1) {
+			return $intersectionArguments[0];
+		} else {
+			return call_user_func_array('array_intersect', $intersectionArguments);
+		}
+	}
+
+	/**
+	 *
+	 * @param string $graphIri
+	 * @param mixed $subject (string or null)
+	 * @param mixed $predicate (string or null)
+	 * @param mixed $object (string or null)
+	 *
+	 * @throws Erfurt_Exception
+	 *
+	 * @return int The number of statements deleted
+	 */
+	public function getMatchingStatements($graphIri, $subject, $predicate, $object) {
+		if ((!isset($this->graphs[$graphIri]))
+				|| (!empty($subject) && empty($this->statementsBySubject[$subject]))
+				|| (!empty($predicate) && empty($this->statementsByPredicate[$predicate]))
+				|| (!empty($object) && empty($this->statementsByObject[$object]))
+			) {
+
+			return array();
+		}
+
+		$matchingStatements = $this->intersectWithoutNullValues(
+			$this->statementsByGraph[$graphIri],
+			$this->statementsBySubject[$subject],
+			$this->statementsByPredicate[$predicate],
+			$this->statementsByObject[$object]
+		);
+
+		$statements = array();
+		foreach ($matchingStatements as $statementHash) {
+			$statements[] = $this->statements[$statementHash];
+		}
+		return $statements;
+	}
+
+	/**
+	 *
+	 * @param string $graphIri
+	 * @param mixed $subject (string or null)
+	 * @param mixed $predicate (string or null)
+	 * @param mixed $object (string or null)
+	 * @param array $options
+	 *
+	 * @throws Erfurt_Exception
+	 *
+	 * @return int The number of statements deleted
+	 */
+	public function deleteMatchingStatements($graphIri, $subject, $predicate, $object, array $options = array()) {
+		$matchingStatements = $this->getMatchingStatements();
+	}
+
+	/**
+	 * Deletes statements in an array from the graph specified by $graphIri.
+	 *
+	 * @param string $graphIri
+	 * @param array  $statementsArray
+	 */
+	public function deleteMultipleStatements($graphIri, array $statementsArray) {
+		// TODO: Implement deleteMultipleStatements() method.
+	}
+
+}
+
+?>
---
+++ Tests/Unit/Store/Adapter/MemoryTest.php
@@ -0,0 +1,180 @@
+<?php
+declare(ENCODING = 'utf-8') ;
+namespace Erfurt\Tests\Unit\Store\Adapter;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2011 Andreas Wolf <andreas.wolf@ikt-werk.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+
+class MemoryTest extends \Erfurt\Tests\BaseTestCase {
+	/**
+	 * @var \Erfurt\Store\Adapter\Memory
+	 */
+	protected $fixture;
+
+	public function setUp() {
+		$this->fixture = new \Erfurt\Store\Adapter\Memory();
+
+		$mockedObjectManager = $this->getMock('\Erfurt\Object\ObjectManager');
+		$mockedObjectManager->expects($this->any())->method('create')->will($this->returnValue($this->getMock('\Erfurt\Domain\Model\Rdf\Graph')));
+		$this->fixture->injectObjectManager($mockedObjectManager);
+	}
+
+	/**
+	 * Returns a randomized statement for use with the addMultipleStatements method of the Adapter
+	 */
+	protected function getRandomStatement() {
+		return array(
+			uniqid() => array( // subject
+				uniqid() => array( // predicate
+					uniqid() // object
+				)
+			)
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function createGraphCreatesAndStoresNewGraph() {
+		// TODO: is this a correct IRI for a graph?
+		$iri = 'http://example.org/some/random/uri/' . uniqid() . '#';
+
+		$mockedGraph = $this->getMock('\Erfurt\Domain\Model\Rdf\Graph');
+		$mockedObjectManager = $this->getMock('\Erfurt\Object\ObjectManager');
+		$mockedObjectManager->expects($this->atLeastOnce())->method('create')->with($this->anything(), $this->equalTo($iri))->will($this->returnValue($mockedGraph));
+		$this->fixture->injectObjectManager($mockedObjectManager);
+
+		$this->fixture->createGraph($iri);
+		$this->assertTrue($this->fixture->isGraphAvailable($iri));
+		$this->assertSame($mockedGraph, $this->fixture->getGraph($iri));
+	}
+
+	/**
+	 * @test
+	 * @depends createGraphCreatesAndStoresNewGraph
+	 */
+	public function statementsCanBeAddedAndRetrieved() {
+		$mockedObjectManager = $this->getMock('\Erfurt\Object\ObjectManager');
+		$mockedObjectManager->expects($this->atLeastOnce())->method('create')->will($this->returnValue($this->getMock('\Erfurt\Domain\Model\Rdf\Graph')));
+		$this->fixture->injectObjectManager($mockedObjectManager);
+
+		$graphIri = uniqid();
+		$s = uniqid();
+		$p = uniqid();
+		$o1 = uniqid(); $o2 = uniqid();
+		$statements = array(
+			$s => array( // subject 1
+				$p => array( // predicate 1
+					$o1, // object 1
+					$o2  // object 2
+				)
+			)
+		);
+
+		$this->fixture->createGraph($graphIri);
+		$randomIri = uniqid();
+		$this->fixture->createGraph($randomIri);
+
+		$this->fixture->addMultipleStatements($graphIri, $statements);
+			// add another statement to some random graph to make sure that we only get results for the desired graph
+		$this->fixture->addMultipleStatements($randomIri, array(uniqid() => array(uniqid() => array(uniqid()))));
+		$statements = $this->fixture->getMatchingStatements($graphIri, NULL, NULL, NULL);
+
+		$this->assertEquals(2, count($statements), 'We expected to get two statements, but we got ' . count($statements));
+		$this->assertEquals(array($graphIri, $s, $p, $o1), array_values($statements[0]));
+		$this->assertEquals(array($graphIri, $s, $p, $o2), array_values($statements[1]));
+	}
+
+	/**
+	 * @test
+	 */
+	public function addMultipleStatementsThrowsExceptionIfGraphDoesNotExist() {
+		$this->setExpectedException('\Erfurt\Store\Adapter\Exception', '', 1308322086);
+
+		$this->fixture->addMultipleStatements(uniqid(), array());
+	}
+
+	/**
+	 * @test
+	 */
+	public function getMatchingStatementsReturnsOnlyStatementsForGivenGraph() {
+		$firstIri = uniqid(); $secondIri = uniqid();
+		$s1 = uniqid();$p1 = uniqid();$o1 = uniqid();
+		$s2 = uniqid();$p2 = uniqid();$o2 = uniqid();
+
+		$this->fixture->createGraph($firstIri);
+		$this->fixture->createGraph($secondIri);
+		$this->fixture->addStatement($firstIri, $s1, $p1, $o1);
+		$this->fixture->addStatement($secondIri, $s2, $p2, $o2);
+
+		$firstGraphStatements = $this->fixture->getMatchingStatements($firstIri, NULL, NULL, NULL);
+		$this->assertEquals(1, count($firstGraphStatements));
+		$this->assertEquals(array('g' => $firstIri, 's' => $s1, 'p' => $p1, 'o' => $o1), $firstGraphStatements[0]);
+	}
+
+	/**
+	 * Adds various statements to a graph and returns them
+	 *
+	 * @test
+	 * @depends statementsCanBeAddedAndRetrieved
+	 */
+	public function getMatchingStatementsReturnsAllMatchingStatements() {
+			// we create a simple graph here with one subject, two predicates and two objects
+		$graphIri = uniqid();
+		$subject = uniqid(); $unusedSubject = uniqid();
+		$predicate1 = uniqid(); $predicate2 = uniqid();
+		$object1 = uniqid(); $object2 = uniqid();
+		$statements = array(
+			$subject => array(
+				$predicate1 => array($object1, $object2),
+				$predicate2 => array($object1)
+			)
+		);
+		$this->fixture->createGraph($graphIri);
+		$this->fixture->addMultipleStatements($graphIri, $statements);
+
+		$statementsForUnusedSubject = $this->fixture->getMatchingStatements($graphIri, $unusedSubject, NULL, NULL);
+		$statementsForPredicate1 = $this->fixture->getMatchingStatements($graphIri, NULL, $predicate1, NULL);
+		$statementsForPredicate2 = $this->fixture->getMatchingStatements($graphIri, NULL, $predicate2, NULL);
+		$statementsForObject1 = $this->fixture->getMatchingStatements($graphIri, NULL, NULL, $object1);
+		$statementsForObject2 = $this->fixture->getMatchingStatements($graphIri, NULL, NULL, $object2);
+
+		$this->assertEquals(0, count($statementsForUnusedSubject));
+
+		$this->assertEquals(2, count($statementsForPredicate1));
+		$this->assertContains(array('g' => $graphIri, 's' => $subject, 'p' => $predicate1, 'o' => $object1), $statementsForPredicate1);
+		$this->assertContains(array('g' => $graphIri, 's' => $subject, 'p' => $predicate1, 'o' => $object2), $statementsForPredicate1);
+
+		$this->assertEquals(1, count($statementsForPredicate2));
+		$this->assertContains(array('g' => $graphIri, 's' => $subject, 'p' => $predicate2, 'o' => $object1), $statementsForPredicate2);
+
+		$this->assertEquals(2, count($statementsForObject1));
+		$this->assertContains(array('g' => $graphIri, 's' => $subject, 'p' => $predicate1, 'o' => $object1), $statementsForObject1);
+		$this->assertContains(array('g' => $graphIri, 's' => $subject, 'p' => $predicate2, 'o' => $object1), $statementsForObject1);
+
+		$this->assertEquals(1, count($statementsForObject2));
+		$this->assertContains(array('g' => $graphIri, 's' => $subject, 'p' => $predicate1, 'o' => $object2), $statementsForObject2);
+	}
+}
+
